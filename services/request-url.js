3ee3bd31590c4a1e5da83c6ec5d9cdfe1e4f3b14
huyld <d.huy93@gmail.com>
2018-06-16 18:05:57 +0700

    feat(requestUrl): use GET method for tinyurl domain
    Abort after receiving the original URL

--- services/request-url.js
+++ services/request-url.js
@@ -5,16 +5,39 @@
  * @returns
  */
 function requestUrl(sourceUrl) {
+    var hostname = getHostName(sourceUrl);
     return new Promise((resolve, reject) => {
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = event => {
-            if (event.target.readyState == 2) {
-                if (event.target.status == 200) {
-                    resolve(event.target.responseURL);
-                }
-            }
-        };
-        xhr.open('HEAD', sourceUrl, true);
+        xhr.onreadystatechange = onStateChangeCallback.bind(this, resolve, reject, xhr);
+        switch (hostname) {
+            case TINYURL:
+                // tinyurl.com doesn't allow HEAD method
+                xhr.open('GET', sourceUrl, true);
+                break;
+
+            default:
+                xhr.open('HEAD', sourceUrl, true);
+                break;
+        }
         xhr.send();
     });
+}
+
+/**
+ * Callback for readystatechange event
+ *
+ * @param {*} resolve
+ * @param {*} reject
+ * @param {*} xhr
+ * @param {*} event
+ */
+function onStateChangeCallback(resolve, reject, xhr, event) {
+    if (event.target.readyState == 2) {
+        if (event.target.status == 200) {
+            resolve(event.target.responseURL);
+
+            // Only the header is necessary
+            xhr.abort();
+        }
+    }
 }
