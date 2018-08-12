99f6bb78c12676d83467570b12b941c97f49a3a7
huyld <3625371+huyld@users.noreply.github.com>
2018-08-12 13:00:10 +0700

    feat(main-bg): manage ports connected to content scripts

--- background-scripts/main-background.js
+++ background-scripts/main-background.js
@@ -1,7 +1,8 @@
+// List of ports connected to content scripts
+var portFromContentScript = [];
+
 // Listen to connection from content script
 chrome.runtime.onConnect.addListener(onContentScriptConnected);
-
-var portFromContentScript;
 
 /**
  * Listener to connection from content script
@@ -9,11 +10,12 @@ var portFromContentScript;
  * @param {any} port
  */
 function onContentScriptConnected(port) {
-    portFromContentScript = port;
-    portFromContentScript.postMessage({ greeting: "hi there content script!" });
-    portFromContentScript.onMessage.addListener(msg => {
+    portFromContentScript.push(port);
+    port.postMessage({ greeting: "hi there content script!" });
+    port.onMessage.addListener(msg => {
         onReceivingMsgContentScript(msg);
     });
+    port.onDisconnect.addListener(onPortDisconnected);
 }
 
 /**
@@ -22,5 +24,22 @@ function onContentScriptConnected(port) {
  * @param {any} msg
  */
 function onReceivingMsgContentScript(msg) {
-    console.log("In background script, received message from content script")
+    console.log("In background script, received message from content script", msg);
+}
+
+/**
+ * Listener to disconnection from content script (tab closed or error occurs)
+ *
+ * @param {*} port
+ */
+function onPortDisconnected(port) {
+    if (chrome.runtime.lastError) {
+        console.error('URL Revealer: port disconnected due to an error', chrome.runtime.lastError.message);
+    } else {
+        console.debug('URL Revealer: port from tab %s disconnected', port.sender.tab.id, port);
+    }
+    const index = portFromContentScript.indexOf(port);
+    if (index > -1) {
+        portFromContentScript.splice(index, 1);
+    }
 }
