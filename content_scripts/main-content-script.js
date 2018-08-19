38d9be877112c959ac66e7543fe168860872b2dd
huyld <3625371+huyld@users.noreply.github.com>
2018-08-19 17:38:23 +0700

    refactor(tooltip): move creating tooltips logic to main content script

--- content_scripts/main-content-script.js
+++ content_scripts/main-content-script.js
@@ -1,3 +1,5 @@
+// List of all anchor elements in the current page
+var anchors = document.getElementsByTagName('a');
 
 // Port connected to background script
 var backgroundPort;
@@ -6,6 +8,8 @@ var backgroundPort;
 
     // Create connection with background script
     connectToBackgroundScript(onReceivingMsgBackgroundScript);
+
+    setUrlAttributeForAnchors();
 })();
 
 /**
@@ -57,4 +61,21 @@ function sendURLToBackground(url) {
             }
         );
     });
+}
+
+/**
+ * Send url of all anchor tags to background for further process.
+ * If the response.success is true, this means the URL belongs to a supported domain,
+ * then set the result as anchor's attribute.
+ *
+ */
+function setUrlAttributeForAnchors() {
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+        sendURLToBackground(anchor.href).then(response => {
+            if (response.success) {
+                anchor.setAttribute('data-url-tooltip', response.orignalURL);
+            }
+        });
+    }
 }
