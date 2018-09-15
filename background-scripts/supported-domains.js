737aeb42b762e39229f55122c74c2681d3eae6cb
huyld <3625371+huyld@users.noreply.github.com>
2018-09-15 18:55:38 +0700

    feat(supported-domains): add more supported domains ✨
    2tu.us, budurl.com, doiop.com, fat.ly, ff.im, korta.nu, migre.me, ow.ly,
    p.ly, tighturl.com and xrl.us

--- background-scripts/supported-domains.js
+++ background-scripts/supported-domains.js
@@ -1,52 +1,74 @@
+const _2TU_US = '2tu.us';
 const _7ly = '7.ly';
 const ALTURL_COM = 'alturl.com';
 const B_LINK = 'b.link';
 const BIT_LY = 'bit.ly';
+const BUDURL_COM = 'budurl.com';
 const CHILP_IT = 'chilp.it';
+const DOIOP_COM = 'doiop.com';
 const EXAMPLES_NEWS = 'examples.news';
 const EXAMPLES_SOCIAL = 'examples.social';
 const INFORMATION_CODES = 'information.codes';
+const FAT_LY = 'fat.ly';
+const FF_IM = 'ff.im';
 const FLIC_KR = 'flic.kr';
 const GOO_GL = 'goo.gl';
 const GIT_IO = 'git.io';
 const IS_GD = 'is.gd';
+const KORTA_NU = 'korta.nu';
 const MERKY_DE = 'merky.de';
+const MIGRE_ME = 'migre.me';
+const OW_LY = 'ow.ly';
+const P_LY = 'p.ly';
 const SLNK_ME = 'slnk.me';
 const TCRN_CH = 'tcrn.ch';
 const TO_LY = 'to.ly';
+const TIGHTURL_COM = 'tighturl.com';
 const TINY_CC = 'tiny.cc';
 const TINYURL_COM = 'tinyurl.com';
 const TNY_IM = 'tny.im';
 const U_NU = 'u.nu';
 const URL_IE = 'url.ie';
 const URLCUT_COM = 'urlcut.com';
+const XRL_US = 'xrl.us';
 const Y2U_BE = 'y2u.be';
 const YATUC_COM = 'yatuc.com';
 const YEP_IT = 'yep.it';
 
 var supportedDomains = [
+    _2TU_US,
     _7ly,
     ALTURL_COM,
     B_LINK,
     BIT_LY,
+    BUDURL_COM,
     CHILP_IT,
+    DOIOP_COM,
     EXAMPLES_NEWS,
     EXAMPLES_SOCIAL,
+    FAT_LY,
+    FF_IM,
     FLIC_KR,
     GOO_GL,
     GIT_IO,
     IS_GD,
+    KORTA_NU,
     INFORMATION_CODES,
     MERKY_DE,
+    MIGRE_ME,
+    OW_LY,
+    P_LY,
     SLNK_ME,
     TCRN_CH,
     TO_LY,
+    TIGHTURL_COM,
     TINY_CC,
     TINYURL_COM,
     TNY_IM,
     U_NU,
     URL_IE,
     URLCUT_COM,
+    XRL_US,
     Y2U_BE,
     YATUC_COM,
     YEP_IT
