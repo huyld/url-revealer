cec22097348c760638d82e5f20fb168f5bfee8e2
huyld <3625371+huyld@users.noreply.github.com>
2018-09-15 12:35:05 +0700

    feat(supported-domains): add more supported domains ✨
    chilp.it, merky.de, slnk.me, tcrn.ch, to.ly, u.nu, url.ie, urlcut.com
    yatuc.com and yep.it

--- background-scripts/supported-domains.js
+++ background-scripts/supported-domains.js
@@ -2,6 +2,7 @@ const _7ly = '7.ly';
 const ALTURL_COM = 'alturl.com';
 const B_LINK = 'b.link';
 const BIT_LY = 'bit.ly';
+const CHILP_IT = 'chilp.it';
 const EXAMPLES_NEWS = 'examples.news';
 const EXAMPLES_SOCIAL = 'examples.social';
 const INFORMATION_CODES = 'information.codes';
@@ -9,16 +10,26 @@ const FLIC_KR = 'flic.kr';
 const GOO_GL = 'goo.gl';
 const GIT_IO = 'git.io';
 const IS_GD = 'is.gd';
+const MERKY_DE = 'merky.de';
+const SLNK_ME = 'slnk.me';
+const TCRN_CH = 'tcrn.ch';
+const TO_LY = 'to.ly';
 const TINY_CC = 'tiny.cc';
 const TINYURL_COM = 'tinyurl.com';
 const TNY_IM = 'tny.im';
+const U_NU = 'u.nu';
+const URL_IE = 'url.ie';
+const URLCUT_COM = 'urlcut.com';
 const Y2U_BE = 'y2u.be';
+const YATUC_COM = 'yatuc.com';
+const YEP_IT = 'yep.it';
 
 var supportedDomains = [
     _7ly,
     ALTURL_COM,
     B_LINK,
     BIT_LY,
+    CHILP_IT,
     EXAMPLES_NEWS,
     EXAMPLES_SOCIAL,
     FLIC_KR,
@@ -26,8 +37,17 @@ var supportedDomains = [
     GIT_IO,
     IS_GD,
     INFORMATION_CODES,
+    MERKY_DE,
+    SLNK_ME,
+    TCRN_CH,
+    TO_LY,
     TINY_CC,
     TINYURL_COM,
     TNY_IM,
-    Y2U_BE
+    U_NU,
+    URL_IE,
+    URLCUT_COM,
+    Y2U_BE,
+    YATUC_COM,
+    YEP_IT
 ];
