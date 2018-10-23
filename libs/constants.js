b41c8cafa5a9d3a9bf6e6e8848618a1f71b1012e
huyld <huyld@users.noreply.github.com>
2018-10-24 02:19:26 +0700

    feat(content): detect changes in specific sections and handle its anchors ✨
    Facebook and Youtube domain

--- libs/constants.js
+++ libs/constants.js
@@ -14,12 +14,33 @@ const CMD_CHECK_AND_HANDLE_URL = 'check-and-handle-url';
 
 // REGEX
 const FB_REDIRECT_URL_REGEX = /[^=]*=(.+)(?=&h=)/;
+const YOUTUBE_REDIRECT_URL_REGEX = /^.*q=([^&]*)&?/;
 const HOSTNAME_REGEX = /:\/\/(www[0-9]?\.)?(.[^/:]+)/i;
 
 // DOMAINS
 const FACEBOOK_COM = 'facebook.com';
 const FACEBOOK_REDIRECT_URL = 'l.facebook.com/l.php?u=';
+const YOUTUBE_COM = 'youtube.com';
+const YOUTUBE_REDIRECT_URL = '/redirect';
 
 // CONFIGURATION
 const WAITING_INTERVAL = 2000;
 const WAITING_RETRY_MAX = 10;
+
+const TARGET_DOMS = {
+    'facebook.com': [
+        {   // Feed
+            selector: 'div[role="feed"]',
+            mutable: true
+        }
+    ],
+    'youtube.com': [
+        {   // Description
+            selector: 'div#meta-contents div#container'
+        },
+        {   // Comments
+            selector: 'ytd-comments#comments div#contents',
+            mutable: true
+        }
+    ]
+};
