532307c070fb6285dcb2abd0eb7f8d75b08a3d18
huyld <huyld@users.noreply.github.com>
2018-12-15 13:25:26 +0700

    feat(requestUrl): future proof if more services don't allow HEAD method ✨

--- background-scripts/request-url.js
+++ background-scripts/request-url.js
@@ -2,23 +2,56 @@
  * Return the final destination URL from the shortened URL
  *
  * @param {string} sourceUrl
- * @returns
+ * @returns {Promise<string>} the source URL if success, the error message if fail
  */
 function requestUrl(sourceUrl) {
     var hostname = getHostName(sourceUrl);
     return new Promise((resolve, reject) => {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = onStateChangeCallback.bind(this, resolve, reject, xhr);
+        let method = '';
         switch (hostname) {
             case BITLY_IS:
             case DOIOP_COM:
             case TINYURL_COM:
-                xhr.open('GET', sourceUrl, true);
+                // These services don't allow HEAD method
+                method = 'GET';
                 break;
             default:
-                xhr.open('HEAD', sourceUrl, true);
+                method = 'HEAD';
                 break;
         }
+
+        requestUrlHelper(sourceUrl, method)
+            .then(result => resolve(result))
+            .catch(error => {
+                if (error.code === 405) {
+                    // Future proof: if services other than the above don't allow HEAD method
+                    // Retry to make another request with GET method
+                    method = 'GET';
+                    requestUrlHelper(sourceUrl, method)
+                        .then(result => resolve(result))
+                        .catch(error => {
+                            // Service still doesn't allow GET method, time to give up 😩
+                            reject(new Error(error.message));
+                        });
+                } else {
+                    reject(new Error(error.message));
+                }
+            });
+    });
+}
+
+/**
+ * Send request to `sourceUrl` using `method`
+ *
+ * @param {string} sourceUrl
+ * @param {string} method `HEAD`, `GET`
+ * @returns
+ */
+function requestUrlHelper(sourceUrl, method) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = onStateChangeCallback.bind(this, resolve, reject, xhr);
+        xhr.open(method, sourceUrl, true);
         xhr.send();
     });
 }
@@ -33,7 +66,10 @@ function requestUrl(sourceUrl) {
  */
 function onStateChangeCallback(resolve, reject, xhr, event) {
     if (event.target.status >= 400) {
-        reject(new Error(`HTTP status ${event.target.status}`));
+        reject({
+            code: event.target.status,
+            message: `HTTP status ${event.target.status}`
+        });
         xhr.abort();
     } else {
         if (event.target.readyState == 2) {
